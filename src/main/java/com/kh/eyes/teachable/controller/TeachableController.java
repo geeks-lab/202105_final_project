d7620ddf759f6b17dd9a8b77a5191680c45e64d4
jihyon2775 <jihyon2775@naver.com>
2021-04-02 16:59:00 +0900

    [지현]teachableController 수정

--- src/main/java/com/kh/eyes/teachable/controller/TeachableController.java
+++ src/main/java/com/kh/eyes/teachable/controller/TeachableController.java
@@ -1,5 +1,21 @@
 package com.kh.eyes.teachable.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("teach")
 public class TeachableController {
+	
+	@GetMapping("food")
+	public String food() {
+		return "teachable/teachFood";
+	}
+	
+	@GetMapping("product")
+	public String product() {
+		return "teachable/teachProduct";
+	}
 
 }
