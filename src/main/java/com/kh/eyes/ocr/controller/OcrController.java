d17fdf01debb2c8087147b4c19da32385f278237
hksthgml <hksthgml@gmail.com>
2021-05-03 21:57:50 +0900

    [지현,소희]문서기능추가

--- src/main/java/com/kh/eyes/ocr/controller/OcrController.java
+++ src/main/java/com/kh/eyes/ocr/controller/OcrController.java
@@ -1,15 +1,42 @@
 package com.kh.eyes.ocr.controller;
 
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kh.eyes.ocr.service.OcrService;
 
 @Controller
 @RequestMapping("ocr")
 public class OcrController {
-	 @GetMapping("document")
-	   public String instead() {
-	      return "ocr/documentOcr";
-}
 	
+	private final OcrService ocrService;
+	
+	public OcrController(OcrService ocrService) {
+		this.ocrService = ocrService;
+	}
+	
+	@GetMapping("document")
+	public String instead() {
+		return "ocr/documentOcr";
+	}
+
+	@PostMapping("response")
+	@ResponseBody
+	public ResponseEntity<String> getTextFromOCR(@RequestBody Map<String,Object> bodyObj) {				
+		String response = ocrService.getTextFromOcr(bodyObj);
+		HttpHeaders responseHeaders = new HttpHeaders(); 
+		responseHeaders.add("Content-Type", "text/plain; charset=utf-8");
+
+		return new ResponseEntity<String>(response, responseHeaders,HttpStatus.OK);
+	}
+
 }
--- src/main/webapp/resources/js/ocr.js
+++ src/main/webapp/resources/js/ocr.js
@@ -19,15 +19,6 @@ async function ocrText(){ //텍스트인식함수
         "version": "V1"      
 	};
 	
-	let response = await fetch("/teach/ocr", {
-		"method": "POST",
-		"headers":{'Content-Type': 'application/json'},
-		"body":  JSON.stringify(bodyObj)            
-	}).then(response => response.json())
-    	.then(data => {
-			document.getElementById('teachContent').insertAdjacentHTML('beforeend', "<br>"); //티처블 결과와 줄바꿈 처리
-            for (var i = 0; i < data["images"][0]["fields"].length; i++) { //결과 출력 insertAdjacentHTML활용하여 티처블 결과에 추가로 삽입
-                document.getElementById('teachContent').insertAdjacentHTML('beforeend', data["images"][0]["fields"][i]["inferText"] + " ");
-          	}
-      	})
+	ocrResponse(bodyObj);
+	
 }
